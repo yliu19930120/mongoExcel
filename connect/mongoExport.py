379622daf082f12da1413054a1cae4fd5d027495
yliu19930120 <yliu19930120@163.com>
2018-06-07 17:51:55 +0800

    增加一个分字段导出方法

--- connect/mongoExport.py
+++ connect/mongoExport.py
@@ -6,31 +6,55 @@ from connect.timer import Timer
 
 class MongoExport(object):
 
-    def __init__(self,path,fileName):
-        self.path = path+fileName
-        self.client =  MongoClient()
-
-    def buildBook(self,fileds):
-        self.fields = fileds
+    def __init__(self, path, fileName):
+        self.path = path + fileName
+        self.client = MongoClient()
         self.workbook = xlsxwriter.Workbook(self.path, {'nan_inf_to_errors': True})  # 处理无穷大
-        self.worksheet = self.workbook.add_worksheet()
+
+    def buildSheet(self, name='sheet0'):
+        worksheet = self.workbook.add_worksheet(name)
         # 设置行头
         for j in range(len(self.fields)):
-            self.worksheet.write(0, j, self.fields[j])
+            worksheet.write(0, j, self.fields[j])
+            j = j + 1
+        return worksheet
 
-    def export(self,dbName,colName):
-        timer = Timer(time.time())
+    def export(self, dbName, colName, fields ,filter={},sort =[('_id',1)]):
+        start = time.time()
+        self.fields = fields
+        worksheet = self.buildSheet()
         db = self.client.get_database(dbName)
-        job = db.get_collection(colName)
-        rlist = job.find()
-        i =0
+        col = db.get_collection(colName)
+        rlist = col.find(filter).sort(sort)
+        rowNum = 1
         for r in rlist:
-            j = 0
-            for j in range(len(self.fields)):
-                self.worksheet.write(i, j, r[self.fields[j]])
-            i = i + 1
+            rowNum = self.write(r, worksheet, rowNum)
         self.workbook.close()
-        timer.print()
+        print('耗时:', time.time() - start)
+
+    def write(self, record, worksheet, rowNum):
+        j = 0
+        for j in range(len(fields)):
+            worksheet.write(rowNum, j, record[fields[j]])
+
+        return rowNum + 1
+
+    def exportGroupByField(self, dbName, colName, fields, fieldName):
+        self.fields = fields
+        start = time.time()
+        d = {}
+        rowNumD = {}
+        db = self.client.get_database(dbName)
+        col = db.get_collection(colName)
+        rlist = col.find()
+        for r in rlist:
+            key = r[fieldName]
+            if (key not in d.keys()):
+                d[key] = self.buildSheet(key)
+                rowNumD[key] = 1
+            rowNumD[key] = self.write(r, d[key], rowNumD[key])
+        self.workbook.close()
+        print('耗时:', time.time() - start)
 
 if __name__ == '__main__':
     export = MongoExport("E:/Python练习/excel/","job2.xlsx")
